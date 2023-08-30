2fe71f2f36e0800c0a56c88b3ecd8760395f3daf
Kacper Sokołowski <kontakt@kaapa.pl>
2023-08-30 17:40:25 +0200

    Lekcja o stylach globalnych

---
+++ front-end/src/App.jsx
@@ -0,0 +1,8 @@
+import "./styles/theme.css";
+import "./styles/globals.css";
+
+function App() {
+    return <p>Witaj w naszym sklepie!</p>;
+}
+
+export default App;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,39 +1,9 @@
-import "./styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { MainPage } from "./views/MainPage/MainPage";
-import { Favourites } from "./views/Favourites/Favourites";
-import { ProductsList } from "./views/ProductsList/ProductsList";
-import { ProductDetails } from "./views/ProductDetails/ProductDetails";
-import { Cart } from "./views/Cart/Cart";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainPage />,
-    },
-    {
-        path: "/koszyk",
-        element: <Cart />,
-    },
-    {
-        path: "/ulubione",
-        element: <Favourites />,
-    },
-    {
-        path: "/produkty",
-        element: <ProductsList />,
-    },
-    {
-        path: "/produkt",
-        element: <ProductDetails />,
-    },
-]);
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 );
